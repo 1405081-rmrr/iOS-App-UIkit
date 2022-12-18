a4bd2198918673ccc146ee83defc06f360e65527
1405081-rmrr <1405081.rmrr@ugrad.cse.buet.ac.bd>
2022-12-18 12:13:54 +0600

    Updated

---
+++ TableViewDelegate/Roktim_30023/Roktim_30023/FirstCustomTable.swift
@@ -0,0 +1,31 @@
+//
+//  FirstCustomTable.swift
+//  Roktim_30023
+//
+//  Created by Bjit on 7/12/22.
+//
+
+import UIKit
+
+class FirstCustomTable: UITableViewCell {
+
+    
+    @IBOutlet weak var imgView: UIImageView!
+    
+    @IBOutlet weak var labelView3: UILabel!
+    @IBOutlet weak var imgView3: UIImageView!
+    @IBOutlet weak var labelView2: UILabel!
+    @IBOutlet weak var imgView2: UIImageView!
+    @IBOutlet weak var labelView: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
